6e782e44f510982a465f7d33bca476cf3faec281
chayutpong <chayutpong@creditok.co>
2023-02-12 20:09:01 +1100

    build: block public access in s3

--- tf/modules/frontend/bucket.tf
+++ tf/modules/frontend/bucket.tf
@@ -3,6 +3,15 @@ resource "aws_s3_bucket" "resume_bucket" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_public_access_block" "block_public_all_access" {
+  bucket = aws_s3_bucket.resume_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 output "bucket_name" {
   value = aws_s3_bucket.resume_bucket.id
 }
