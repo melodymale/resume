248157a446f848d4ec8efd9ac0625b94e454c992
chayutpong <chayutpong@creditok.co>
2023-03-02 09:47:53 +1100

    build: change creating gateway http method to POST on lambda function

--- tf/modules/backend/gateway.tf
+++ tf/modules/backend/gateway.tf
@@ -14,7 +14,7 @@ resource "aws_api_gateway_resource" "lambda" {
 
 resource "aws_api_gateway_method" "lambda" {
   authorization = "NONE"
-  http_method   = "GET"
+  http_method   = "POST"
   resource_id   = aws_api_gateway_resource.lambda.id
   rest_api_id   = aws_api_gateway_rest_api.lambda.id
 }
