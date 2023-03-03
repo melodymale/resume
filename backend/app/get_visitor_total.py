ad584edb572b82267dee32e8a78e631d405f2bb8
chayutpong <chayutpong@creditok.co>
2023-03-03 14:05:33 +1100

    fix: logging info

--- backend/app/get_visitor_total.py
+++ backend/app/get_visitor_total.py
@@ -1,7 +1,11 @@
+import logging
+
 from db_client import DBClient
+
+logger = logging.getLogger(__name__)
 
 
 def get_visitor_total(db_client: DBClient, key: dict) -> int:
-    print("get visitor total")
+    logger.info("get visitor total")
     total = db_client.get_item(key=key)
     return total
--- backend/app/update_visitor_total.py
+++ backend/app/update_visitor_total.py
@@ -1,7 +1,11 @@
+import logging
+
 from db_client import DBClient
+
+logger = logging.getLogger(__name__)
 
 
 def update_visitor_total(db_client: DBClient, key: dict):
-    print("update visitor total")
+    logger.info("update visitor total")
     total = db_client.update_item(key=key)
     return total
