8b5bcbd86bd908a0be4d9390ad494151908a1007
chayutpong <chayutpong@creditok.co>
2023-03-04 09:14:09 +1100

    test: not use relative dot anymore

--- backend/test/test_update_visitor_total.py
+++ backend/test/test_update_visitor_total.py
@@ -1,6 +1,5 @@
 from app.update_visitor_total import update_visitor_total
-
-from .dummy_db_client import DummyDBClient
+from dummy_db_client import DummyDBClient
 
 
 def test_update_visitor_total():
