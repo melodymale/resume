ff969c8c239e8c7bbfa61f43cbacd21b228a922d
chayutpong <chayutpong@creditok.co>
2023-03-03 21:42:26 +1100

    build: add options method for CORS

--- tf/modules/backend/gateway.tf
+++ tf/modules/backend/gateway.tf
@@ -19,6 +19,67 @@ resource "aws_api_gateway_method" "lambda" {
   rest_api_id   = aws_api_gateway_rest_api.lambda.id
 }
 
+resource "aws_api_gateway_method_response" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.lambda.id
+  resource_id = aws_api_gateway_resource.lambda.id
+  http_method = aws_api_gateway_method.lambda.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_method" "options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.lambda.id
+  resource_id   = aws_api_gateway_resource.lambda.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "options_200" {
+  rest_api_id = aws_api_gateway_rest_api.lambda.id
+  resource_id = aws_api_gateway_resource.lambda.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.lambda.id
+  resource_id = aws_api_gateway_resource.lambda.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = jsonencode(
+      {
+        statusCode = 200
+      }
+    )
+  }
+}
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.lambda.id
+  resource_id = aws_api_gateway_resource.lambda.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = aws_api_gateway_method_response.options_200.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,x-api-key'",
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = aws_api_gateway_rest_api.lambda.id
   resource_id             = aws_api_gateway_resource.lambda.id
@@ -42,7 +103,9 @@ resource "aws_api_gateway_deployment" "lambda" {
     redeployment = sha1(jsonencode([
       aws_api_gateway_resource.lambda.id,
       aws_api_gateway_method.lambda.id,
+      aws_api_gateway_method.options_method.id,
       aws_api_gateway_integration.integration.id,
+      aws_api_gateway_integration.options_integration.id
     ]))
   }
 
@@ -148,6 +211,5 @@ resource "aws_lambda_permission" "api_gw" {
 
 output "base_url" {
   description = "Base URL for API Gateway stage."
-
-  value = aws_api_gateway_stage.lambda.invoke_url
+  value       = aws_api_gateway_stage.lambda.invoke_url
 }
