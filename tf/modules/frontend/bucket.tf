94afb307ee33c15e7c90fa5f32879466944dbc9c
chayutpong <chayutpong@creditok.co>
2023-08-14 11:41:37 +1000

    terraform: change s3 to be public

--- tf/modules/frontend/bucket.tf
+++ tf/modules/frontend/bucket.tf
@@ -3,11 +3,46 @@ resource "aws_s3_bucket" "resume_bucket" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_website_configuration" "s3_web_hosting" {
+  bucket = aws_s3_bucket.resume_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "block_public_all_access" {
   bucket = aws_s3_bucket.resume_bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.resume_bucket.id
+  policy = data.aws_iam_policy_document.allow_public_access.json
+}
+
+data "aws_iam_policy_document" "allow_public_access" {
+  statement {
+    sid = "AllowPublicAccess"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.resume_bucket.arn}/*"
+    ]
+  }
 }
