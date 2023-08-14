dd305b057db007f40cb04f17109b7fe32fccdb77
chayutpong <chayutpong@creditok.co>
2023-08-14 12:01:35 +1000

    terraform: delete aws_s3_bucket_public_access_block
    This is because default value is false and we need it like that

--- tf/modules/frontend/bucket.tf
+++ tf/modules/frontend/bucket.tf
@@ -15,20 +15,6 @@ resource "aws_s3_bucket_website_configuration" "s3_web_hosting" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "block_public_all_access" {
-  bucket = aws_s3_bucket.resume_bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_policy" "allow_public_access" {
-  bucket = aws_s3_bucket.resume_bucket.id
-  policy = data.aws_iam_policy_document.allow_public_access.json
-}
-
 data "aws_iam_policy_document" "allow_public_access" {
   statement {
     sid = "AllowPublicAccess"
@@ -45,4 +31,9 @@ data "aws_iam_policy_document" "allow_public_access" {
       "${aws_s3_bucket.resume_bucket.arn}/*"
     ]
   }
+}
+
+resource "aws_s3_bucket_policy" "allow_public_access" {
+  bucket = aws_s3_bucket.resume_bucket.id
+  policy = data.aws_iam_policy_document.allow_public_access.json
 }
