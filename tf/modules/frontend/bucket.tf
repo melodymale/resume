da8d4e673cbc6d5b48ac61cb172ef3acfc98ff14
chayutpong <chayutpong@creditok.co>
2023-08-14 11:49:09 +1000

    terraform: attach public access policy to bucket

--- tf/modules/frontend/bucket.tf
+++ tf/modules/frontend/bucket.tf
@@ -24,7 +24,7 @@ resource "aws_s3_bucket_public_access_block" "block_public_all_access" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+resource "aws_s3_bucket_policy" "allow_public_access" {
   bucket = aws_s3_bucket.resume_bucket.id
   policy = data.aws_iam_policy_document.allow_public_access.json
 }
